703ddbf1cc11d58e80a800c8a81e1164e758e7e0
mikarpasoftsas <mikarpasoftsas@gmail.com>
2018-09-24 17:54:16 -0500

    El directorio cache no se versiona

--- cache/smarty/templates_c/%%7C^7C8^7C8076E0%%DashletFooter.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.31, created on 2018-09-24 21:43:54
-         compiled from include/Dashlets/DashletFooter.tpl */ ?>
-</div><div class="mr"></div></div><div class="ft"><div class="bl"></div><div class="ft-center"></div><div class="br"></div></div>
