61cc8ebf835c619a5a2fe18b68d8d5e551428eed
Gerardo <gr@creacttive.com>
2018-12-09 12:44:23 -0300

    messages.js se modifica textos en espanol. fb-messages-reply.php se agrega html entity decode. messages.css se agrega pre line.

--- custom/modules/Opportunities/js/messages.js
+++ custom/modules/Opportunities/js/messages.js
@@ -1,10 +1,10 @@
 var messageConfig = {
     title: 'Mensajes',
-    url : window.location.protocol + '//' + window.location.host + '/index.php?entryPoint=getMessagesFb',
-    resultsNotFound : 'No hay mensajes',
+    url: window.location.protocol + '//' + window.location.host + '/index.php?entryPoint=getMessagesFb',
+    resultsNotFound: 'No hay mensajes',
     form: {
-        input : {'placeholder' : 'Type message ...'},
-        button: {'label' : 'Send'},
+        input: {'placeholder': 'Ingresar mensaje ...'},
+        button: {'label': 'Enviar'},
         actionUrl: window.location.protocol + '//' + window.location.host + '/index.php?entryPoint=saveMessageFb'
     }
 };
@@ -14,120 +14,130 @@ var $messagesNoResults = null;
 var $messagesContent = null;
 var messagesPage = 1;
 $(document).ready(function () {
-    createChatBox(function(){
-        loadMoreData();
+    createChatBox(function () {
+        loadMoreData(function(totalRecords){
+            // if first call records not found destroy chatbox.
+            if(totalRecords == 0){
+                $('.chat-box').remove();
+            }
+        });
     });
 });
 
-function loadMoreData() {
+function loadMoreData(callback) {
     messagesLoaded = false;
-	var $idpreinforme = $("#formDetailView").find('input[name="record"]').val();	
-	var $account_id = $("#account_id").val();
+    var $idpreinforme = $("#formDetailView").find('input[name="record"]').val();
+    var $account_id = $("#account_id").val();
     $.ajax(
-        {
-            url: messageConfig.url,
-			data : {				
-				'messagesPage':messagesPage,
-				'idpreinforme':$idpreinforme,
-				'idcontacto':$account_id
-			},			
-            type: "get",
-            dataType: "json",
-            beforeSend: function ()
             {
-                $messagesNoResults.hide();
-                $messagesLoading.show();
-            }
-        })
-        .done(function (data)
-        {
-            if(data.messages.length > 0){
-                messagesPage++;
-                $.each(data.messages, function(i,item){
-                    $messagesContent.append(loadMessage(item));
-                });
-                messagesLoaded = true;
-                $messagesLoading.hide();
-            }else{
-                $messagesLoading.hide(function(){
-                    $messagesNoResults.fadeIn();
-                });
-                setTimeout(function(){
+                url: messageConfig.url,
+                data: {
+                    'messagesPage': messagesPage,
+                    'idpreinforme': $idpreinforme,
+                    'idcontacto': $account_id
+                },
+                type: "get",
+                dataType: "json",
+                beforeSend: function ()
+                {
+                    $messagesNoResults.hide();
+                    $messagesLoading.show();
+                }
+            })
+            .done(function (data)
+            {
+                var total = data.messages.length;
+                if (total > 0) {
+                    messagesPage++;
+                    $.each(data.messages, function (i, item) {
+                        $messagesContent.append(loadMessage(item));
+                    });
                     messagesLoaded = true;
-                }, 120000);
-            }
-        })
-        .fail(function (jqXHR, ajaxOptions, thrownError)
-        {
-            console.log('ERROR load more messages', thrownError);
-            messagesLoaded = true;
-        });
+                    $messagesLoading.hide();
+                } else {
+                    $messagesLoading.hide(function () {
+                        $messagesNoResults.fadeIn();
+                    });
+                    setTimeout(function () {
+                        messagesLoaded = true;
+                    }, 120000);
+                }
+                
+                if (callback != null && typeof callback == 'function') {
+                    callback(total);
+                }
+            })
+            .fail(function (jqXHR, ajaxOptions, thrownError)
+            {
+                console.log('ERROR load more messages', thrownError);
+                messagesLoaded = true;
+            });
 }
 
-function loadMessage(obj){
+function loadMessage(obj) {
     var $html = null;
-    if(obj.type === 'sent'){
+    if (obj.type === 'sent') {
         $html = $('<div class="direct-chat-msg right">' +
                 '<div class="direct-chat-info clearfix">' +
-                    '<span class="direct-chat-name pull-right">'+ obj.from.name +'</span>' +
-                    '<span class="direct-chat-timestamp pull-left">'+ obj.created_at +'</span>' +
+                '<span class="direct-chat-name pull-right">' + obj.from.name + '</span>' +
+                '<span class="direct-chat-timestamp pull-left">' + obj.created_at + '</span>' +
                 '</div>' +
-                '<img class="direct-chat-img" src="'+ obj.from.avatar +'" alt="'+ obj.from.name +'">' +
+                '<img class="direct-chat-img" src="' + obj.from.avatar + '" alt="' + obj.from.name + '">' +
                 '<div class="direct-chat-text">' + obj.text + '</div>' +
-            '</div>');
-    }else{
+                '</div>');
+    } else {
         $html = $('<div class="direct-chat-msg">' +
                 '<div class="direct-chat-info clearfix">' +
-                    '<span class="direct-chat-name pull-left">'+ obj.from.name +'</span>' +
-                    '<span class="direct-chat-timestamp pull-right">'+ obj.created_at +'</span>' +
+                '<span class="direct-chat-name pull-left">' + obj.from.name + '</span>' +
+                '<span class="direct-chat-timestamp pull-right">' + obj.created_at + '</span>' +
                 '</div>' +
-                '<img class="direct-chat-img" src="'+ obj.from.avatar +'" alt="'+ obj.from.name +'">' +
+                '<img class="direct-chat-img" src="' + obj.from.avatar + '" alt="' + obj.from.name + '">' +
                 '<div class="direct-chat-text">' + obj.text + '</div>' +
-            '</div>');
+                '</div>');
     }
     return $html;
 }
 
-function createChatBox(callback){
+function createChatBox(callback) {
     $('body').append('<div class="chat-box">' +
-        '<div class="row bootstrap snippets">' +
+            '<div class="row bootstrap snippets">' +
             '<div class="col-md-12 col-xs-12 col-sm-12">' +
-                '<div class="box box-warning direct-chat direct-chat-warning">' +
-                    '<div class="box-header with-border" data-widget="collapse">' +
-                        '<h3 class="box-title">'+ messageConfig.title +'</h3>' +
-                        '<div class="box-tools pull-right">' +
-                            '<span class="btn-box-tool"><i class="fa fa-2x fa-comments"></i></span>' +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="chat-box-section">' +
-                        '<div class="loading row">' +
-                            '<div class="col-md-12 text-center">' +
-                                '<i class="fa fa-spinner fa-spin fa-3x fa-fw" style="font-size:16px;"></i>' +
-                            '</div>' +
-                        '</div>' +
-                        '<div class="noresults row">' +
-                            '<div class="col-md-12 text-center">' +
-                                '<div class="alert alert-info">' +
-                                    '<i class="fa fa-info-circle"></i> ' + messageConfig.resultsNotFound +
-                                '</div>' +
-                            '</div>' +
-                        '</div>' +
-                        '<div class="box-body"><div class="direct-chat-messages"></div></div>' +
-                        '<div class="box-footer">' +
-                            '<form action="" method="post">' +
-                                '<div class="input-group">' +
-                                    '<input required="required" type="text" name="message" placeholder="'+ messageConfig.form.input.placeholder +'" class="form-control" autocomplete="off">' +
-                                    '<span class="input-group-btn">' +
-                                        '<button type="submit" class="btn btn-warning btn-flat">'+ messageConfig.form.button.label +'</button>' +
-                                    '</span>' +
-                                '</div>' +
-                            '</form>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>' +
+            '<div class="box box-warning direct-chat direct-chat-warning">' +
+            '<div class="box-header with-border" data-widget="collapse">' +
+            '<h3 class="box-title">' + messageConfig.title + '</h3>' +
+            '<div class="box-tools pull-right">' +
+            '<span class="btn-box-tool"><i class="fa fa-2x fa-comments"></i></span>' +
             '</div>' +
-        '</div>' +
-    '</div>');
+            '</div>' +
+            '<div class="chat-box-section">' +
+            '<div class="loading row">' +
+            '<div class="col-md-12 text-center">' +
+            '<i class="fa fa-spinner fa-spin fa-3x fa-fw" style="font-size:16px;"></i>' +
+            '</div>' +
+            '</div>' +
+            '<div class="noresults row">' +
+            '<div class="col-md-12 text-center">' +
+            '<div class="alert alert-info">' +
+            '<i class="fa fa-info-circle"></i> ' + messageConfig.resultsNotFound +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '<div class="box-body"><div class="direct-chat-messages"></div></div>' +
+            '<div class="box-footer">' +
+            '<form action="" method="post">' +
+            '<div class="input-group">' +
+            '<textarea required="required" name="message" placeholder="' + messageConfig.form.input.placeholder + '" class="form-control"></textarea>' +
+            '<span class="input-group-btn">' +
+            '<button type="submit" class="btn btn-warning btn-flat">' + messageConfig.form.button.label + '</button>' +
+            '</span>' +
+            '</div>' +
+            '</form>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>');
 
     $('.box-header[data-widget="collapse"]').click(function () {
         $('.chat-box-section').toggleClass("open-more");
@@ -149,47 +159,49 @@ function createChatBox(callback){
         }
     });
 
-    if(callback != null && typeof callback == 'function'){
+    if (callback != null && typeof callback == 'function') {
         callback();
     }
 }
 
-function sendMessage($form){
-    var $input = $form.find('input[type="text"]');
-	var $idpreinforme = $("#formDetailView").find('input[name="record"]').val();
-	var $account_id = $("#EditView").find('input[id="account_id"]').val();	
-    var $btn = $form.find('[type="submit"]');
-    $input.attr('readonly', true);
-    $btn.attr('disabled', true);
-    $.ajax(
-        {
-            url: messageConfig.form.actionUrl,
-            data : {
-				
-				'text': $input.val(),
-				'idpreinforme':$idpreinforme,
-				'idcontacto':$account_id
-			},
-            type: "post",
-            dataType: "json",
-            beforeSend: function ()
-            {
+function sendMessage($form) {
+    var $input = $form.find('[name="message"]');
+    if($.trim($input.val()).length > 0){
+        var $idpreinforme = $("#formDetailView").find('input[name="record"]').val();
+        var $account_id = $("#EditView").find('input[id="account_id"]').val();
+        var $btn = $form.find('[type="submit"]');
+        $input.attr('readonly', true);
+        $btn.attr('disabled', true);
+        $.ajax(
+                {
+                    url: messageConfig.form.actionUrl,
+                    data: {
 
-            }
-        })
-        .done(function (data)
-        {
-            $input.removeAttr('readonly');
-            $btn.attr('disabled', false);
-            if(data.message != null){
-                $input.val('');
-                $messagesContent.prepend(loadMessage(data.message));
-            }
-        })
-        .fail(function (jqXHR, ajaxOptions, thrownError)
-        {
-            $input.removeAttr('readonly');
-            $btn.attr('disabled', false);
-            console.log('ERROR save message', thrownError);
-        });
+                        'text': $input.val(),
+                        'idpreinforme': $idpreinforme,
+                        'idcontacto': $account_id
+                    },
+                    type: "post",
+                    dataType: "json",
+                    beforeSend: function ()
+                    {
+
+                    }
+                })
+                .done(function (data)
+                {
+                    $input.removeAttr('readonly');
+                    $btn.attr('disabled', false);
+                    if (data.message != null) {
+                        $input.val('');
+                        $messagesContent.prepend(loadMessage(data.message));
+                    }
+                })
+                .fail(function (jqXHR, ajaxOptions, thrownError)
+                {
+                    $input.removeAttr('readonly');
+                    $btn.attr('disabled', false);
+                    console.log('ERROR save message', thrownError);
+                });
+    }
 }
--- custom/modules/mks_MessagesFb/fb-messages-reply.php
+++ custom/modules/mks_MessagesFb/fb-messages-reply.php
@@ -1,37 +1,41 @@
 <?php
+
 if (!defined('sugarEntry') || !sugarEntry)
     die('Not A Valid Entry Point');
 
 class fb_messages_reply {
-    	
-	
-	function executeCurl($url, $jsonData=array()) {
-		//Initiate cURL.
-		$ch = curl_init($url);
-		//Attach our encoded JSON string to the POST fields.
-		if (is_array($jsonData) && count($jsonData) > 0) {
-			//Tell cURL that we want to send a POST request.
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsonData));
-			//Set the content type to application/json
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-		}
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		return curl_exec($ch);
-	}
-	
-	function fb_messages_reply(&$bean, $event, $arguments)
-	{
-			if($bean->type=="sent"){
-			$fillial = BeanFactory::getBean('SecurityGroups',$bean->recipient);
-			$contact = BeanFactory::getBean('Accounts',$bean->sender);
-					
-			$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' . $fillial->fb_page_access_token_c;
-			$jsonData = array(
-				"recipient" => array("id" => $contact->fb_user_id_c),
-				"message" => array("text" => $bean->description)
-			);
-			$this->executeCurl($url, $jsonData);			
-		}
-	}
+
+    function executeCurl($url, $jsonData = array()) {
+        //Initiate cURL.
+        $ch = curl_init($url);
+        //Attach our encoded JSON string to the POST fields.
+        if (is_array($jsonData) && count($jsonData) > 0) {
+            //Tell cURL that we want to send a POST request.
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsonData));
+            //Set the content type to application/json
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        return curl_exec($ch);
+    }
+
+    function fb_messages_reply(&$bean, $event, $arguments) {
+        error_log('fb_messages_reply ' . time());
+        error_log('fb_messages_reply bean: ' . \json_encode($bean));
+        if ($bean->type == "sent") {
+            $fillial = BeanFactory::getBean('SecurityGroups', $bean->recipient);
+            $contact = BeanFactory::getBean('Accounts', $bean->sender);
+
+            $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' . $fillial->fb_page_access_token_c;
+            $jsonData = array(
+                "recipient" => array("id" => $contact->fb_user_id_c),
+                "message" => array("text" => html_entity_decode($bean->description))
+            );
+            error_log('fb_messages_reply url: ' . $url);
+            error_log('fb_messages_reply data: ' . \json_encode($jsonData));
+            $this->executeCurl($url, $jsonData);
+        }
+    }
+
 }
