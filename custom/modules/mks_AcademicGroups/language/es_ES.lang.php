fad8a3ab281bcd138917209d155e1704c65b8f63
mikarpasoftsas <mikarpasoftsas@gmail.com>
2018-12-20 09:42:53 -0600

    20. ABM Matricula. y Nuevo campo vacante disponible

--- custom/modules/mks_AcademicGroups/language/es_ES.lang.php
+++ custom/modules/mks_AcademicGroups/language/es_ES.lang.php
@@ -1,7 +1,8 @@
 <?php
-// created: 2018-11-23 17:31:44
+// created: 2018-12-20 15:38:25
 $mod_strings = array (
   'LBL_MKS_ACADEMICGROUPS_MKS_GROUPMATTERS_1_FROM_MKS_GROUPMATTERS_TITLE' => 'Materias del Grupo',
   'LBL_TYPE' => 'Tipo',
   'LBL_DESCRIPTION' => 'Disponibilidad',
+  'LBL_VACANCIES_AVAILABLE' => 'Vacantes Disponibles',
 );
