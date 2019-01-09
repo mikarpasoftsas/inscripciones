6f4a966fb13d72f19a6d46b94ded2d4586ce468f
mikarpasoftsas <mikarpasoftsas@gmail.com>
2019-01-09 10:11:00 -0600

    Tratamiento al mensaje de respuesta con caracteres especiales

--- custom/modules/mks_MessagesFb/fb-messages-reply.php
+++ custom/modules/mks_MessagesFb/fb-messages-reply.php
@@ -20,8 +20,9 @@ class fb_messages_reply {
 		return curl_exec($ch);
 	}
 	
-	function fb_messages_reply(&$bean, $event, $arguments)
-	{
+	function fb_messages_reply(&$bean, $event, $arguments){
+			error_log('fb_messages_reply ' . time());
+			error_log('fb_messages_reply bean: ' . \json_encode($bean));
 			if($bean->type=="sent"){
 			$fillial = BeanFactory::getBean('SecurityGroups',$bean->recipient);
 			$contact = BeanFactory::getBean('Accounts',$bean->sender);
@@ -29,7 +30,7 @@ class fb_messages_reply {
 			$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' . $fillial->fb_page_access_token_c;
 			$jsonData = array(
 				"recipient" => array("id" => $contact->fb_user_id_c),
-				"message" => array("text" => $bean->description)
+				"message" => array("text" => html_entity_decode($bean->description))
 			);
 			$this->executeCurl($url, $jsonData);			
 		}
