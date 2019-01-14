c7b3631f02835f4c0086674f6154a3e0b5f9ecb9
Lucas <lr@creacttive.com>
2019-01-14 16:05:58 -0300

    test bugfix inlinesave oncomplete

--- custom/modules/mks_Registration/js/detailview.js
+++ custom/modules/mks_Registration/js/detailview.js
@@ -1,5 +1,6 @@
 function mks_refinances_mks_registration_inlineSave_onComplete(){
     console.log('mks_refinances_mks_registration_inlineSave_onComplete is called. Then redirect');
+    document.location.reload(true);
 }
 
 $( document ).ready(function() {
