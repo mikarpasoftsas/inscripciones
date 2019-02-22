866a6afe1c31a809a8089cdc709a1bae0dcbb672
Lucas <lr@creacttive.com>
2019-02-22 19:55:56 -0300

    update ftp server

--- ftp-transfer-production-server.sh
+++ ftp-transfer-production-server.sh
@@ -1,9 +1,15 @@
-git add *
-git commit -m "Commit Files and Folders"
-git config git-ftp.url ftp://162.241.139.215/
-git config git-ftp.user repository@logos.igi-la.com
-git config git-ftp.password R3posiTor1
+# git add *
+# git commit -m "Commit Files and Folders"
+
+git config git-ftp.production.user ftp://162.241.139.215/
+git config git-ftp.production.url repository@logos.igi-la.com
+git config git-ftp.production.password R3posiTor1
+
+# git config git-ftp.url ftp://162.241.139.215/
+# git config git-ftp.user repository@logos.igi-la.com
+# git config git-ftp.password R3posiTor1
 # The first time enabled this line for INIT and push must be disabled.
-git ftp init
+git ftp init -s production
+# git ftp init
 # git ftp push
 sleep 20
