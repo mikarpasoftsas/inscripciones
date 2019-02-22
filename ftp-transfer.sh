16a36a9746da10d616ec516159902be76cf465b2
Lucas <lr@creacttive.com>
2019-02-22 19:45:18 -0300

    delete file

--- ftp-transfer.sh
+++
@@ -1,7 +0,0 @@
-git config git-ftp.url ftp://168.197.51.58/
-git config git-ftp.user igi-crm@creacttive.com
-git config git-ftp.password aLAmeda1
-# The first time enabled this line for INIT and push must be disabled.
-# git ftp push
-git ftp push
-sleep 20
